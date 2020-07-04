3cbddce33e83cb2338aa7cf0ffe72b294352b8fe
Ivan <63588501+ivan-vitenko@users.noreply.github.com>
2020-07-04 10:59:50 +0300

    Створює незрозумілий вигляд мобільного меню

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,19 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileMenuRefBullshit = document.querySelector('[data-menu-bullshit]');
+  const mobileMenuRefHeroBullshit = document.querySelector(
+    '[data-menu-hero-bullshit]',
+  );
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
     menuBtnRef.classList.toggle('is-open');
+    mobileMenuRefBullshit.classList.toggle('header--some-bullshit');
+    mobileMenuRefHeroBullshit.classList.toggle('hero--some-bullshit');
+
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
